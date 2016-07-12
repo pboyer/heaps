50266a6f575255038978199cef05827cc706f1c3
Peter Boyer <peter.b.boyer@gmail.com>
2016-07-12 15:05:15 -0400

    Binary heaps

---
+++ binary.go
@@ -0,0 +1,90 @@
+package heap
+
+import ( "fmt" )
+
+type binaryHeap struct {
+    vals []int
+}
+
+func makeBinaryHeap() *binaryHeap {
+    return &binaryHeap{ 
+        vals : make([]int, 0, 10),
+    }
+}
+
+func left(i int) int {
+    return 2 * i + 1
+}
+
+func right(i int) int {
+    return 2 * i + 2
+}
+
+func par(i int) int {
+    return (i - 1) / 2
+}
+
+func (h *binaryHeap) insert(val int) {
+    h.vals = append(h.vals, val)
+
+    i := len(h.vals) - 1
+
+    for {
+        if i == 0 {
+            return
+        }
+
+        parI := par(i)
+        if h.vals[parI] > h.vals[i] {
+            h.vals[parI], h.vals[i] = h.vals[i], h.vals[parI]
+            i = parI
+        } else {
+            return
+        }
+    }
+}
+
+func (h *binaryHeap) dequeue() (int, error) {
+    if len(h.vals) == 0 {
+        return -1, fmt.Errorf("Empty heap")
+    }
+
+    val := h.vals[0]
+    h.heapify()
+
+    return val, nil
+}
+
+func (h *binaryHeap) heapify(){
+    h.vals[0] = h.vals[len(h.vals) - 1]
+    h.vals = h.vals[0:len(h.vals) - 1]
+
+    if len(h.vals) == 0 {
+        return
+    }
+
+    i := 0
+
+    for {
+        leftI := left(i)
+        rightI := right(i)
+        smallest := i
+
+        if leftI < len(h.vals) && h.vals[i] > h.vals[leftI] { 
+            smallest = leftI
+        }
+
+        if rightI < len(h.vals) && h.vals[i] > h.vals[rightI] {
+            smallest = rightI
+        }
+
+        if smallest != i {
+            h.vals[smallest], h.vals[i] = h.vals[i], h.vals[smallest]
+            i = smallest
+        } else {
+            return
+        }
+    }
+}
+
+
---
+++ binary_test.go
@@ -0,0 +1,34 @@
+package heap
+
+import (
+    "testing"
+    "math/rand"
+)
+
+func TestBinary(t *testing.T) {
+    for r := 0; r < 10; r++ {
+        c := 1000
+        h := makeBinaryHeap()
+
+        for i := 0; i < c; i++ {
+            h.insert(rand.Int())
+        }
+
+        last := 0
+        for i := 0; i < c/2; i++ {
+            v, err := h.dequeue()
+
+            if err != nil {
+                t.Fatalf("unexpected error %v", err)
+            }
+
+            if i == 0 {
+                continue
+            }
+
+            if v < last {
+                t.Fatalf("violation of heap property",)
+            }
+        }
+    }
+}
